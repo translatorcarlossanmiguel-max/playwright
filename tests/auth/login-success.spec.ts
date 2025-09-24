a5844de6f2b55a2c3ebac4ea4fcf8ad6cc0ede72
carlossanmiguel <carlos.sanmiguel@thinktum.com>
2025-09-24 17:10:12 -0300

    Playwright test1

--- tests/auth/login-success.spec.ts
+++ tests/auth/login-success.spec.ts
@@ -5,7 +5,4 @@ test('Logeo exitoso con credenciales válidas', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
   await loginPage.loginWithValidUser();
-  
-  // ...verificaciones de login exitoso...
- // await expect(page).toHaveURL(/.*dashboard/);
 });
