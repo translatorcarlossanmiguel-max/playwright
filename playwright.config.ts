4b64c761edad27182c06b172a804667776036fb7
carlossanmiguel <carlos.sanmiguel@thinktum.com>
2025-09-29 11:26:20 -0300

    Playwright test11

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from '@playwright/test';
 
+const isCI = !!process.env.CI;
+
 export default defineConfig({
-    retries: 0,
+    retries: 2,
     workers: 2,
     fullyParallel: true,
     use: {
         baseURL: 'https://www.saucedemo.com',
-        headless: false,
+        headless: isCI ? true : false,
         viewport: { width: 1280, height: 720 },
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
