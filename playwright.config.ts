707b89b04a6a0151e5f97d2c6f4bc92647eccbb8
carlossanmiguel <carlos.sanmiguel@thinktum.com>
2025-09-29 19:28:04 -0300

    Read Me Update file & yml Update9

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,16 +2,35 @@ import { defineConfig } from '@playwright/test';
 
 const isCI = !!process.env.CI;
 
+const QA = process.env.ENV || 'QA';
+
+const PROD = process.env.ENV || 'PROD';
+
+const envUrls: Record<string, string> = {
+    DEV: 'https://dev.saucedemo.com',
+    QA: 'https://qa.saucedemo.com',
+    PROD: 'https://www.saucedemo.com'
+};
+
+const ENV = (process.env.ENV || 'QA').toUpperCase();
+const baseURL = envUrls[ENV] || envUrls.QA;
+
 export default defineConfig({
     retries: 2,
     workers: 2,
     fullyParallel: true,
     use: {
-        baseURL: 'https://www.saucedemo.com',
+        baseURL,
         headless: isCI ? true : false,
         viewport: { width: 1280, height: 720 },
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         trace: 'retain-on-failure',
+        launchOptions: {
+            env: {
+                ...process.env,
+                ENV
+            }
+        },
     }
 });
